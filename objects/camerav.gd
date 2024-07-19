d6f49a93ceb495968edb12b51037fd3d5524b575
nulta <un5450@outlook.com>
2024-07-19 14:03:21 +0900

    fix: CameraV.punch_to does not cancel in-progress punch

--- objects/camerav.gd
+++ objects/camerav.gd
@@ -6,6 +6,8 @@ const BASE_PUNCH_STRENGTH = 20
 
 @export var focus_node: CanvasItem = null
 
+var _punch_tween: Tween = null
+
 
 func _process(_delta: float) -> void:
 	if is_instance_valid(focus_node) and "position" in focus_node:
@@ -14,8 +16,11 @@ func _process(_delta: float) -> void:
 			position += focus_node.size / 2
 
 
-func punch_to(dir: Vector2 = Vector2.UP, time: float = 0.4, strength: float = 1.0) -> void:
-	var new_offset: = dir * (BASE_PUNCH_STRENGTH * strength)
-	var tween: = create_tween().chain().set_trans(Tween.TRANS_QUINT)
-	tween.tween_property(self, "offset", new_offset, time * 2/10.0)
-	tween.tween_property(self, "offset", Vector2.ZERO, time * 8/10.0)
+func punch_to(dir: Vector2 = Vector2.UP, time: float = 0.3, strength: float = 1.0) -> void:
+	offset = dir * (BASE_PUNCH_STRENGTH * strength)
+
+	if is_instance_valid(_punch_tween):
+		_punch_tween.kill()
+
+	_punch_tween = create_tween().set_trans(Tween.TRANS_QUINT)
+	_punch_tween.tween_property(self, "offset", Vector2.ZERO, time)
